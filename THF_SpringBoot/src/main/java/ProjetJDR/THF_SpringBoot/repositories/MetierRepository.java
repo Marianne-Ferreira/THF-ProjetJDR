04cbb73d0ce5bd8558b38f34f6daa8db4d3368a5
Layla <adillayla@gmail.com>
2022-05-25 14:54:21 +0200

    Modif Métier Services et Personnage Services

--- THF_SpringBoot/src/main/java/ProjetJDR/THF_SpringBoot/repositories/MetierRepository.java
+++ THF_SpringBoot/src/main/java/ProjetJDR/THF_SpringBoot/repositories/MetierRepository.java
@@ -1,10 +1,21 @@
 package ProjetJDR.THF_SpringBoot.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ProjetJDR.THF_SpringBoot.entity.Compte;
 import ProjetJDR.THF_SpringBoot.entity.Metier;
+import ProjetJDR.THF_SpringBoot.entity.Personnage;
 
 public interface MetierRepository extends JpaRepository <Metier, Long>{
+	
+	@Query("select m from Metier m where m.prenom=:prenom")
+	Optional<Metier> findByPrenom(@Param("prenom") String prenom);
+
+	@Query("select m from Metier m where m.nom=:nom")
+	Optional<Metier> findByNom(@Param("nom") String nom);
 
 }
