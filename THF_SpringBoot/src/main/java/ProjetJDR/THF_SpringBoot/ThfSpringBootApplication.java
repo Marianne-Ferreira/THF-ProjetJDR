c4ed98ffa16bcd68ffe652a415d22b729199a317
Marianne Ferreira <marianne.ferreira@outlook.fr>
2022-05-25 11:38:27 +0200

    Création nouveau projet SpringBoot

---
+++ THF_SpringBoot/src/main/java/ProjetJDR/THF_SpringBoot/ThfSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ProjetJDR.THF_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThfSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThfSpringBootApplication.class, args);
+	}
+
+}
---
+++ THF_SpringBoot/src/test/java/ProjetJDR/THF_SpringBoot/ThfSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetJDR.THF_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThfSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
