24861b5a48c723bb79cee2d181b935684bd5aef3
Layla <adillayla@gmail.com>
2022-06-03 16:48:40 +0200

    Modif CompteRestController + Ajout AuthRestControler

--- THF_SpringBoot/src/main/java/ProjetJDR/THF_SpringBoot/services/CompteService.java
+++ THF_SpringBoot/src/main/java/ProjetJDR/THF_SpringBoot/services/CompteService.java
@@ -77,4 +77,9 @@ public class CompteService implements UserDetailsService{
 	}
 
 	
+	public boolean checkLoginExist(String login) {
+		return compteRepository.findByPseudo(login).isPresent();
+	}
+	
+	
 }
