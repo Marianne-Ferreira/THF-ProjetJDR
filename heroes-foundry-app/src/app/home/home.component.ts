fd2e7f5ba4fef7af064852e9de9aaac4e1821ac7
Layla <adillayla@gmail.com>
2022-06-09 08:26:43 +0200

    Login du homeComponent

--- heroes-foundry-app/src/app/home/home.component.ts
+++ heroes-foundry-app/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Compte } from './../model/compte';
+import { AuthService } from './../services/auth.service';
+
 
 @Component({
   selector: 'app-home',
@@ -6,14 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  login = '';
+  password = '';
+  showError = false;
+  message = '';
 
-  login = 'Pseudo ou adresse mail'
-  password = 'Mot de passe';
-  submit = 'Allons-y !';
-  stay = 'Rester connecté.e';
-  password_forgot = 'Mot de passe oublié';
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submit(form: any) {
+    this.authService.auth(this.login, this.password).subscribe({
+      next: (compte: Compte) => {
+        this.showError = false;
+        sessionStorage.setItem('token', btoa(`${this.login}:${this.password}`));
+        sessionStorage.setItem('compte', JSON.stringify(compte));
+        this.router.navigateByUrl('/home');
+      },
+      error: (error: any) => {
+        console.log(error);
+        this.message = "Le pseudo ou le mot de passe est incorrect, veuillez ré-essayer.";
+        this.showError = true;
+      },
+    });
   }
 
 }
